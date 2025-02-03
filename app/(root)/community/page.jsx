40dd6da6caf73a332819ea4dd910b16ed3d51e13
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2025-02-03 20:11:44 +0700

    change to tailwind css

--- app/(root)/community/page.jsx
+++ app/(root)/community/page.jsx
@@ -1,40 +1,36 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import mealIcon from '@/assets/icons/meal.png';
-import communityIcon from '@/assets/icons/community.png';
-import eventsIcon from '@/assets/icons/events.png';
-import classes from './page.module.css';
+import mealIcon from "@/assets/icons/meal.png";
+import communityIcon from "@/assets/icons/community.png";
+import eventsIcon from "@/assets/icons/events.png";
 
 export default function CommunityPage() {
-    return (
-        <>
-            <header className={classes.header}>
-                <h1 className={"text-4xl font-bold leading-tight"}>
-                    One shared passion: <span className={classes.highlight}>Food</span>
-                </h1>
-                <p>Join our community and share your favorite recipes!</p>
-            </header>
-            <main className={classes.main}>
-                <h2>Community Perks</h2>
+  return (
+    <>
+      <header className={"gap-12 mt-12 mx-auto mb-20 container max-w-[75vh] text-gray-50 text-2xl text-center"}>
+        <h1 className={"text-4xl font-bold leading-tight font-montserrat"}>
+          One shared passion: <span className={"bg-gradient-to-r to-amber-600 from-orange-600 bg-clip-text background-clip-text"}>Food</span>
+        </h1>
+        <p>Join our community and share your favorite recipes!</p>
+      </header>
+      <main className={"container mx-auto text-center"}>
+        <h2 className="text-3xl font-bold mb-12 text-gray-50 font-montserrat">Community Perks</h2>
 
-                <ul className={classes.perks}>
-                    <li>
-                        <Image src={mealIcon} alt="A delicious meal" />
-                        <p>Share & discover recipes</p>
-                    </li>
-                    <li>
-                        <Image src={communityIcon} alt="A crowd of people, cooking" />
-                        <p>Find new friends & like-minded people</p>
-                    </li>
-                    <li>
-                        <Image
-                            src={eventsIcon}
-                            alt="A crowd of people at a cooking event"
-                        />
-                        <p>Participate in exclusive events</p>
-                    </li>
-                </ul>
-            </main>
-        </>
-    );
+        <ul className={"list-none my-12 p-0 mx-0"}>
+          <li className={"flex flex-col items-center gap-4 my-12"}>
+            <Image src={mealIcon} className={"object-contain w-32 aspect-square"} alt="A delicious meal" />
+            <p className={"font-montserrat text-2xl font-bold m-0 text-gray-50"}>Share & discover recipes</p>
+          </li>
+          <li className={"flex flex-col items-center gap-4 my-12"}>
+            <Image src={communityIcon} className={"object-contain w-32 aspect-square"} alt="A crowd of people, cooking" />
+            <p className={"font-montserrat text-2xl font-bold m-0 text-gray-50"}>Find new friends & like-minded people</p>
+          </li>
+          <li className={"flex flex-col items-center gap-4 my-12"}>
+            <Image src={eventsIcon} className={"object-contain w-32 aspect-square"} alt="A crowd of people at a cooking event" />
+            <p className={"font-montserrat text-2xl font-bold m-0 text-gray-50"}>Participate in exclusive events</p>
+          </li>
+        </ul>
+      </main>
+    </>
+  );
 }
